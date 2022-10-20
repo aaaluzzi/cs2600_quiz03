ab2dfc8e787b812813b16be8c170138d4d001698
aaaluzzi <aaaluzzi@cpp.edu>
2022-10-19 20:05:46 -0700

    Subproblem a

--- EmployeeOne.c
+++ EmployeeOne.c
@@ -1,8 +1,7 @@
 #include <string.h>
 #include "Employee.h"
 
-PtrToEmployee searchEmployeeByNumber(PtrToConstEmployee ptr, int tableSize, long targetNumber)
-{
+PtrToEmployee searchEmployeeByNumber(PtrToConstEmployee ptr, int tableSize, long targetNumber) {
 	const PtrToConstEmployee endPtr = ptr + tableSize;
 	for (; ptr < endPtr; ptr++) { //search until end of table  ptr++ will increment by what??
 		if (ptr->number == targetNumber) //check if it equals the Employee number 
@@ -13,8 +12,7 @@ PtrToEmployee searchEmployeeByNumber(PtrToConstEmployee ptr, int tableSize, long
 }
 
 //Essentially the same functionality as above but comparing strings to check if equal 
-PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int tableSize, char *targetName)
-{
+PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int tableSize, char *targetName) {
 	const PtrToConstEmployee endPtr = ptr + tableSize;
 	for (; ptr < endPtr; ptr++) {
 		if (strcmp(ptr->name, targetName) == 0)
@@ -22,4 +20,22 @@ PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int tableSize, char *
 	}
 	return NULL;
 
+}
+
+PtrToEmployee searchEmployeeByPhone(PtrToConstEmployee ptr, int tableSize, char *targetPhone) {
+	const PtrToConstEmployee endPtr = ptr + tableSize;
+	for (; ptr < endPtr; ptr++) {
+		if (strcmp(ptr->phone, targetPhone) == 0)
+			return (PtrToEmployee) ptr;
+	}
+	return NULL;
+}
+
+PtrToEmployee searchEmployeeBySalary(PtrToConstEmployee ptr, int tableSize, double targetSalary) {
+	const PtrToConstEmployee endPtr = ptr + tableSize;
+	for (; ptr < endPtr; ptr++) { 
+		if (ptr->salary == targetSalary) 
+			return (PtrToEmployee) ptr;	
+	}
+	return NULL;	
 }
